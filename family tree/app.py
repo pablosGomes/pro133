5b03c2fc18409a0817b9114882378e55a0b3f4c6
pablosGomes <104365552+pablosGomes@users.noreply.github.com>
2023-10-04 23:13:54 -0300

    Add files via upload

---
+++ family tree/app.py
@@ -0,0 +1,58 @@
+# importando os módulos da biblioteca flask
+from flask import Flask , render_template
+
+# criando a instância da classe Flask, fornecendo a palavra-chave __name__ como argumento
+app = Flask(__name__)
+
+# escreva as rotas usando funções de decorador
+# rota padrão ou 'URL'
+@app.route("/")
+def home():
+
+    nome = "Pablo" # escreva seu nome
+    idade = "13" # escreva sua idade
+
+    return render_template('index.html' , nome = nome , idade = idade)
+
+# defina a rota para a página do pai
+@app.route("/pai")
+def pai():
+
+    nome = "Jose" # escreva seu nome
+    idade = "35" # escreva sua idade
+
+    return render_template('pai.html' , nome = nome , idade = idade)
+
+
+
+
+# defina a rota para a página da mãe
+@app.route("/mae")
+def mae():
+
+    nome = "Debora" # escreva seu nome
+    idade = "35" # escreva sua idade
+
+    return render_template('mae.html' , nome = nome , idade = idade)
+
+
+
+# defina a rota para a página do amigo
+@app.route("/amigo")
+def amigo():
+
+    nome = "Gabriel" # escreva seu nome
+    idade = "13" # escreva sua idade
+
+    return render_template('amigo.html' , nome = nome , idade = idade)
+
+
+
+# adicione outras rotas, se você quiser
+
+
+
+
+# execute o arquivo
+if __name__  ==  '__main__':
+    app.run(debug=True)
